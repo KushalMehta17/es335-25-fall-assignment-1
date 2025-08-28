e22e0865eaa4b27e0320531c5a352e1993b3ecdb
tanishwanve-git <tanish.wanve@iitgn.ac.in>
2025-08-28 17:44:07 +0530

    Update utils.py

--- tree/utils.py
+++ tree/utils.py
@@ -4,44 +4,88 @@ There is no restriction on following the below template, these fucntions are her
 """
 
 import pandas as pd
+import numpy as np
 
 def one_hot_encoding(X: pd.DataFrame) -> pd.DataFrame:
     """
     Function to perform one hot encoding on the input data
     """
-
-    pass
+    return pd.get_dummies(X)
 
 def check_ifreal(y: pd.Series) -> bool:
     """
     Function to check if the given series has real or discrete values
     """
-
-    pass
+    return not y.dtype == "category"
 
 
 def entropy(Y: pd.Series) -> float:
     """
     Function to calculate the entropy
     """
-
-    pass
+    p = Y.value_counts(normalize=True)
+    p[p == 0.0] = 1.0
+    S = -np.sum(p * np.log2(p))
+    return S
 
 
 def gini_index(Y: pd.Series) -> float:
     """
     Function to calculate the gini index
     """
+    p = Y.value_counts(normalize=True)
+    return 1 - np.sum(p * p)
 
-    pass
+def mse(Y: pd.Series) -> float:
+    """
+    Function to calculate mse of data
+    """
+    return np.mean((Y - np.mean(Y)) ** 2)
 
+def get_impurity_function(criterion):
+    fn = None
+    if criterion == 'information_gain':
+        fn = entropy
+    elif criterion == 'gini_index':
+        fn = gini_index
+    elif criterion == 'mse':
+        fn = mse
+    else:
+        raise NotImplementedError("Criterion must be 'information_gain', 'gini_index', or 'mse'")
+    return fn
 
 def information_gain(Y: pd.Series, attr: pd.Series, criterion: str) -> float:
     """
     Function to calculate the information gain using criterion (entropy, gini index or MSE)
+    This will return the optimal split and the information gain for the given attr
     """
-
-    pass
+    fn = None
+    if criterion == 'information_gain':
+        fn = entropy
+    elif criterion == 'gini_index':
+        fn = gini_index
+    elif criterion == 'mse':
+        fn = mse
+    else:
+        raise NotImplementedError("Criterion must be 'information_gain', 'gini_index', or 'mse'")
+    
+    prev_info = fn(Y)
+    attr_values = attr.unique()
+    attr_values.sort()
+    best_gain = 0
+    opt_split = None
+    
+    for value in attr_values:
+        left = Y[attr <= value]
+        right = Y[attr > value]
+        current_info = fn(left) * len(left) / len(Y) + fn(right) * len(right) / len(Y)
+        info_gain = prev_info - current_info
+        if info_gain > best_gain:
+            best_gain = info_gain
+            opt_split = value
+    
+    assert (opt_split is not None or best_gain == 0)    
+    return best_gain, opt_split
 
 
 def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion, features: pd.Series):
@@ -56,8 +100,16 @@ def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion, features: pd.S
     """
 
     # According to wheather the features are real or discrete valued and the criterion, find the attribute from the features series with the maximum information gain (entropy or varinace based on the type of output) or minimum gini index (discrete output).
+    best_information_gain = -np.inf
+    best_attr = None
 
-    pass
+    for feature in features:
+        current_information_gain = information_gain(y, X[feature], criterion)
+        if current_information_gain > best_information_gain:
+            best_information_gain = current_information_gain
+            best_attr = feature
+
+    return best_attr, best_information_gain
 
 
 def split_data(X: pd.DataFrame, y: pd.Series, attribute, value):
@@ -71,7 +123,11 @@ def split_data(X: pd.DataFrame, y: pd.Series, attribute, value):
 
     return: splitted data(Input and output)
     """
-
     # Split the data based on a particular value of a particular attribute. You may use masking as a tool to split the data.
+    assert y.size == X.shape[0]
+    X_left, y_left, X_right, y_right = None, None, None, None
 
-    pass
+    X_left, y_left = X[X[attribute] <= value], y[X[attribute] <= value]
+    X_right, y_right = X[X[attribute] > value], y[X[attribute] > value]
+
+    return X_left, y_left, X_right, y_right
